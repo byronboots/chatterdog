696bd7309bbd2e5f88c2d01b59b83ccaa599bb30
byronboots <bootsbyron@gmail.com>
2023-11-17 23:33:46 -0800

    Update routes and begin question functionality add

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pprint
+import google.generativeai as palm
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -7,9 +11,42 @@ app = Flask(__name__)
 def home():
     return render_template("home.j2")
 
-@app.route('/askdog')
-def askdog():
-    return render_template("ask_dog.j2")
+@app.route('/askdog', methods = ["POST", "GET"])
+def AskDog():
+    
+    if request.method == "GET":
+        return render_template("ask_dog.j2")
+    
+    if request.method == "POST":
+        if request.form.get("Ask_Dog"):
+            palm.configure(api_key=os.environ["PALM_KEY"])
+
+            models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+            model = models[0].name
+
+            # prompt = At the end, make a joke about dogs being better than cats starting with
+            # "You're probably also wondering why dogs are better than cats:"
+
+            prompt_lead = """Answer all questions starting with "WOOF". 
+            Then answer the question intelligently while acting like a dog. 
+            """
+
+            user_input = request.form["user_input"]
+
+            prompt = prompt_lead + " " + user_input
+
+            completion = palm.generate_text(
+                model=model,
+                prompt=prompt,
+                temperature=0,
+                # The maximum length of the response
+                max_output_tokens=800,
+            )
+
+            results = completion.result
+            
+            print(completion.result)
+            return render_template("ask_dog.j2", results=results )
 
 @app.route('/chatdog')
 def chatdog():
